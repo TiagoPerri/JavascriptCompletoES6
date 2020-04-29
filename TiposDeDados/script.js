b10930a7ca097ece8df70e8293f139ebb8c46420
Tiago-Perri <tiagocnsr@gmail.com>
2020-04-28 21:53:06 -0300

    Iniciando os estudos, primeiras aulas

---
+++ TiposDeDados/script.js
@@ -0,0 +1,14 @@
+var frase = 'String 1';
+
+var numstr = '800';
+
+var idade = 18;
+
+var nome = 'Tiago ';
+var sobrenome = 'Perri';
+console.log(nome + sobrenome);
+
+var frase1 = "It's time!!";
+console.log(frase1);
+
+console.log(typeof nome);
---
+++ Variaveis/script.js
@@ -0,0 +1,17 @@
+var nome = 'Tiago';
+console.log(nome);
+
+var idade = 18;
+console.log(idade);
+
+var comidaFavorita;
+comidaFavorita = 'Strogonoff';
+console.log(comidaFavorita);
+
+var time = 'Corinthians', 
+    pais, 
+    cep, 
+    cidade, 
+    planeta = 'Marte';
+
+console.log(time,pais,cep,cidade,planeta);
